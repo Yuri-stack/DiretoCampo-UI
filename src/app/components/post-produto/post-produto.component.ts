130c1566a15d4e7222d232f60ef759c8733d1c3c
vagnermonteiro <monteiro.vagner@gmail.com>
2021-08-10 14:52:01 -0300

    Add Post Produto Componente

---
+++ src/app/components/post-produto/post-produto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-produto',
+  templateUrl: './post-produto.component.html',
+  styleUrls: ['./post-produto.component.css']
+})
+export class PostProdutoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
