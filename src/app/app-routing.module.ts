a58608819e7ac5ba30cb9025b51f1737bcbef17c
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-08-10 14:42:31 -0300

    Add. Account Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProductsUserComponent } from './components/products-user/products-user.component';
 import { StoreComponent } from './components/store/store.component';
 
 const routes: Routes = [
@@ -10,7 +11,8 @@ const routes: Routes = [
 
   {path: 'home', component: HomeComponent},
   {path: 'store', component: StoreComponent},
-  {path: 'cart', component: CartComponent}
+  {path: 'cart', component: CartComponent},
+  {path: 'myProducts', component: ProductsUserComponent}
 
 ];
 
